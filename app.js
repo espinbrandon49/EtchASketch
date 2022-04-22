016fe08ae1c9698bf2e541a310b78631479db667
espinbrandon49 <102924713+espinbrandon49@users.noreply.github.com>
2022-04-21 19:32:10 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,58 @@
 const container = document.querySelector('#container')
 const divs = document.querySelectorAll('div')
-const button = document.getElementById('clearGrid').addEventListener
-  ('click', buttonClick)
 const dives = document.getElementsByClassName('dive')
 
-function buttonClick() {
-let num = prompt('enter a number') 
-for (let i = 0; i < dives.length; i++) {
-  dives[i].style.display = 'none'
-}
+const button = document.getElementById('clearGrid').addEventListener
+  ('click', buttonClick)
+const button2 = document.getElementById('blackClearGrid').addEventListener('click', button2Click)
 
-function squared (num){
-  return num ** 2
-}  
-for (let i = 0; i < squared(num); i++) {
-  let grid = document.createElement('div')
-  grid.className = "dive"
-  grid.textContent = 0
-  grid.style.color = 'rgba(255, 255, 255, 0)'
-  grid.addEventListener('mouseenter', () => grid.style.backgroundColor = 'black'
-  )
-  document.getElementById('container').appendChild(grid)
-}
+function buttonClick() {
+  // select random rainbow color
+  const color = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+  const colorSelected = () => {
+    const randomColor = color[Math.floor((Math.random(0) * 7))]
+    return randomColor
+  }
+
+  // user select rainbow grid size
+  let num = prompt('enter a number')
+  container.setAttribute('style', `grid-template-columns: repeat(${num}, 1fr); grid-template-rows: repeat(${num}, 1fr);`);
+
+  // clear rainbow grid
   for (let i = 0; i < dives.length; i++) {
-    dives[i].style.backgroundColor = 'white'
+    dives[i].style.display = 'none'
+  }
+
+  // rainbow grid created
+  for (let i = 0; i < num * num; i++) {
+    let grid = document.createElement('div')
+    grid.className = "dive"
+    grid.addEventListener('mouseenter', () => grid.style.backgroundColor = colorSelected()
+    )
+    document.getElementById('container').appendChild(grid)
   }
 }
 
-//document.getElementById('container').style.gridTemplateColumns = 'repeat(num 1fr)'
+function button2Click() {
+  // select random color
 
 
-//grid-template-columns: repeat(nu, 1fr)
+  // user select charcoal grid size
+  let num = prompt('enter a number')
+  container.setAttribute('style', `grid-template-columns: repeat(${num}, 1fr); grid-template-rows: repeat(${num}, 1fr);`);
+
+  // clear charcoal grid
+  for (let i = 0; i < dives.length; i++) {
+    dives[i].style.display = 'none'
+  }
+
+  // charcoal grid created
+  for (let i = 0; i < num * num; i++) {
+    let grid = document.createElement('div')
+    grid.className = "dive"
+    grid.addEventListener('mouseenter', () => grid.style.backgroundColor = 'black'
+    )
+    document.getElementById('container').appendChild(grid)
+  }
+}
+
