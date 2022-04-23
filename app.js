f6eb74c9197faca192d78554fef69c44ede95d92
espinbrandon49 <102924713+espinbrandon49@users.noreply.github.com>
2022-04-23 14:14:39 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const container = document.querySelector('#container')
-const divs = document.querySelectorAll('div')
 const dives = document.getElementsByClassName('dive')
-let enterEventCount = 0
-let leaveEventCount = 0
-
-const button = document.getElementById('clearGrid').addEventListener
-  ('click', buttonClick)
+const button = document.getElementById('clearGrid').addEventListener('click', buttonClick)
 const button2 = document.getElementById('blackClearGrid').addEventListener('click', button2Click)
 
 function buttonClick() {
@@ -15,16 +10,13 @@ function buttonClick() {
     const randomColor = color[Math.floor((Math.random(0) * 7))]
     return randomColor
   }
-
   // user select rainbow grid size
   let num = prompt('enter a number')
   container.setAttribute('style', `grid-template-columns: repeat(${num}, 1fr); grid-template-rows: repeat(${num}, 1fr);`);
-
   // clear rainbow grid
   for (let i = 0; i < dives.length; i++) {
     dives[i].style.display = 'none'
   }
-
   // rainbow grid created
   for (let i = 0; i < num * num; i++) {
     let grid = document.createElement('div')
@@ -34,16 +26,15 @@ function buttonClick() {
     document.getElementById('container').appendChild(grid)
   }
 }
+
 function button2Click() {
   // user select charcoal grid size
   let num = prompt('enter a number')
   container.setAttribute('style', `grid-template-columns: repeat(${num}, 1fr); grid-template-rows: repeat(${num}, 1fr);`);
-
   // clear charcoal grid
   for (let i = 0; i < dives.length; i++) {
     dives[i].style.display = 'none'
   }
-
   // charcoal grid created
   for (let i = 0; i < num * num; i++) {
     let grid = document.createElement('div')
@@ -53,7 +44,6 @@ function button2Click() {
 
     grid.addEventListener('mouseover', e => {
       grid.style.backgroundColor = changeOpacity();
-      enterEventCount++;
     })
     //increase shading 10%
     function changeOpacity() {
@@ -70,3 +60,4 @@ function button2Click() {
     }
   }
 }
+
